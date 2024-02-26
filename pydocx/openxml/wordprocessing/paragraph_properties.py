ef57c0891ff840aea154532eafc4bacf3a8bdf55
Regneel Prahalad <regneel.prahalad@onetrust.com>
2024-02-26 10:02:40 -0700

    Revert "Fixed item listing, moved margin-left to <li>"
    This reverts commit 5a4d93a2d8f11553731119781fe1985b322f9e95.

--- pydocx/openxml/wordprocessing/paragraph_properties.py
+++ pydocx/openxml/wordprocessing/paragraph_properties.py
@@ -43,11 +43,3 @@ class ParagraphProperties(XmlModel):
         if start_margin:
             return start_margin
         return 0
-
-    def to_int(self, attribute):
-        # TODO would be nice if this integer conversion was handled
-        # implicitly by the model somehow
-        try:
-            return int(getattr(self, attribute, None))
-        except (ValueError, TypeError):
-            return None
